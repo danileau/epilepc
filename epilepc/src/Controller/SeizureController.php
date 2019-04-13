f07eb20cd3d9449e3720b1fa786dac3764c3f2cb
Danilo <danilo.licitra@gmail.com>
2019-04-13 14:15:05 +0200

    Seizure Controller mit einem Beispiel Twigtemplate erstellt

--- epilepc/src/Controller/SeizureController.php
+++ epilepc/src/Controller/SeizureController.php
@@ -4,9 +4,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class SeizureController
+class SeizureController extends AbstractController
 {
     /**
      * @Route("/")
@@ -19,10 +20,16 @@ class SeizureController
      * @Route("/seizure/{slug}")
      */
     public function show($slug) {
-        return new Response(sprintf(
-            'Future page to show the seizure: %s',
-            $slug)
-        );
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('seizure/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 
 }
