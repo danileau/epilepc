2ee0b358198237f14df96083a56f83b2363d1977
Danilo <danilo.licitra@gmail.com>
2019-04-29 17:44:08 +0200

    Übersicht gestaltet

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -93,6 +93,75 @@ class AppController extends BaseController
         ]);
     }
 
+    /**
+     * @Route("/app/overview", name="app_overview")
+     */
+    public function overview(MedicationRepository $medicationRepository, EventRepository $eventRepository, SeizureRepository $seizureRepository, DiaryentryRepository $diaryentryRepository, UserInterface $user)
+    {
+        /**
+         * Anfallsdaten für die Übersicht aufbereiten
+         */
+        $seizure_data = $seizureRepository->getDiagramSeizureData($user);
+        foreach ($seizure_data as $key => $value) {
+            $seizureDiagramMonth[] = strftime("%B %Y", strtotime($key."-01"));
+            $seizureDiagramCount[] = $value;
+        }
+        $seizureDiagramMonth = array_reverse($seizureDiagramMonth);
+        $seizureDiagramCount = array_reverse($seizureDiagramCount);
+        $seizureMonthJSON = json_encode($seizureDiagramMonth);
+        $seizureValueJSON = json_encode($seizureDiagramCount);
+        $seizures = $seizureRepository->findAllFromUser($user);
+
+        /**
+         * Ereignisdaten für die Diagramme aufbereiten
+         */
+        $event_data = $eventRepository->getDiagramEventData($user);
+        foreach ($event_data as $key => $value) {
+            $eventDiagramCount[] = $value;
+        }
+        $eventDiagramCount = array_reverse($eventDiagramCount);
+        $eventValueJSON = json_encode($eventDiagramCount);
+        $events = $eventRepository->findAllFromUser($user);
+
+        /**
+         * Tagebuchdaten für die Diagramme aufbereiten
+         */
+        $diary_data = $diaryentryRepository->getDiagramDiaryData($user);
+        foreach ($diary_data as $key => $value) {
+            $diaryDiagramCount[] = $value;
+        }
+        $diaryDiagramCount = array_reverse($diaryDiagramCount);
+        $diaryValueJSON = json_encode($diaryDiagramCount);
+        $diaryentrys = $diaryentryRepository->findAllFromUser($user);
+
+        /**
+         * Medikamentendaten für die Diagramme aufbereiten
+         */
+        $medication_data = $medicationRepository->getDiagramMedicationData($user);
+        foreach ($medication_data as $key => $value) {
+            $medicationDiagramCount[] = $value;
+        }
+        $medicationDiagramCount = array_reverse($medicationDiagramCount);
+        $medicationValueJSON = json_encode($medicationDiagramCount);
+        $medications = $medicationRepository->findAllFromUser($user);
+        return $this->render('app/overview.html.twig', [
+            'medication_count' => $medicationRepository->countFindAllFromUser($user),
+            'medication_data' => $medicationValueJSON,
+            'medications' => $medications,
+            'seizure_count' => $seizureRepository->countFindAllFromUser($user),
+            'seizure_month' => $seizureMonthJSON,
+            'seizure_data' => $seizureValueJSON,
+            'seizures' => $seizures,
+            'diaryentry_count' => $diaryentryRepository->countFindAllFromUser($user),
+            'diaryentry_data' => $diaryValueJSON,
+            'diaryentrys' => $diaryentrys,
+            'event_count' => $eventRepository->countFindAllFromUser($user),
+            'event_data' => $eventValueJSON,
+            'events' => $events,
+            'date' => date("d.m.Y")
+        ]);
+    }
+
 
 
 
