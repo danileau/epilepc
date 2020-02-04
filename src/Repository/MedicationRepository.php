719a9c2246e7c99e8979f2a87cd01b2c611d348b
danileau <danilo.licitra@gmail.com>
2020-02-04 10:41:23 +0100

    Datatable Search & sort implementiert - sowie Symfony version update

--- src/Repository/MedicationRepository.php
+++ src/Repository/MedicationRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Medication;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Medication|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MedicationRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Medication::class);
     }
--- src/Repository/SeizureRepository.php
+++ src/Repository/SeizureRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Seizure;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 /**
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SeizureRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Seizure::class);
     }
